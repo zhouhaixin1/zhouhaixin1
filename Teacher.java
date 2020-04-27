c4d6aafad10e19096bf56686fddd35eb64f1741a
zhouhaixin1 <zhouhaixin1@qq.com>
2020-04-27 22:34:40 +0800

    新建文件 Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher{
+	private Integer id;
+	private String name;
+
+	public void setId(Integer id){
+		this.id = id;
+	}
+
+	public Integer getId(){
+		return id;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+}
